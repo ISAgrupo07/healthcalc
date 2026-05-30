04156832fd790e8fce8eb4e906c5faf2471189cb
Yusef Barakat <rafida1441@gmail.com>
2026-05-30 17:35:19 +0200

    refactor: news2 interface
    #80

--- python-project-healthcalc/healthcalc/__init__.py
+++ python-project-healthcalc/healthcalc/__init__.py
@@ -1,6 +1,7 @@
 from .exceptions import InvalidHealthDataException
 from .basal_metabolic_index import BasalMetabolicIndex
 from .ideal_body_weight import IdealBodyWeight
+from .news2_interface import News2
 from .health_calc import HealthCalc
 from .health_calc_impl import HealthCalcImpl
 from .health_calc_decorator import HealthCalcDecorator
@@ -11,6 +12,7 @@ __all__ = [
     'InvalidHealthDataException',
     'BasalMetabolicIndex',
     'IdealBodyWeight',
+    'News2',
     'HealthCalc',
     'HealthCalcImpl',
     'HealthCalcDecorator',
--- python-project-healthcalc/healthcalc/health_calc.py
+++ python-project-healthcalc/healthcalc/health_calc.py
@@ -1,6 +1,7 @@
 from healthcalc.basal_metabolic_index import BasalMetabolicIndex
 from healthcalc.ideal_body_weight import IdealBodyWeight
+from healthcalc.news2_interface import News2
 
 
-class HealthCalc(BasalMetabolicIndex, IdealBodyWeight):
+class HealthCalc(BasalMetabolicIndex, IdealBodyWeight, News2):
     pass
